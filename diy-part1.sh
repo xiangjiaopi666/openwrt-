7bdd75fdeab6d60dfb152f817ef661a4e1a3ed54
xiangjiaopi666 <63852469+xiangjiaopi666@users.noreply.github.com>
2021-02-01 14:47:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,4 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-git clone https://github.com/kenzok8/small.git packages/small
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
